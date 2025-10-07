14e302962c1171ec1a430768063fbbe4b7198511
maniospas <maniospas@hotmail.com>
2025-10-07 16:29:45 +0300

    fixed issue where lists in card html would make sections not show

--- ui/js/card/card.js
+++ ui/js/card/card.js
@@ -11,14 +11,12 @@ document.getElementById('cancel-delete-btn').onclick = function () {
 };
 
 $(document).on("click", ".naccs .menu div", function () {
-    var numberIndex = $(this).index();
-
+    let numberIndex = $(this).index();
     if (!$(this).is("active")) {
         $(".naccs .menu div").removeClass("active");
         $(".naccs ul li").removeClass("active");
-
         $(this).addClass("active");
-        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+        $(".naccs ul").children("li").eq(numberIndex).addClass("active");
 
         /* var listItemHeight = $(".naccs ul")
              .find("li:eq(" + numberIndex + ")")
