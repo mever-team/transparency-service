a316862f7c44d6531b32f0515a06afaa5dc2fbe2
gnikoul <gnikoul@gmail.com>
2025-10-21 13:21:18 +0300

    bug fix

--- package/aicard/service/assistants/prompter.py
+++ package/aicard/service/assistants/prompter.py
@@ -39,11 +39,9 @@ class Prompter(Assistant):
         soup = BeautifulSoup(text, "html.parser")
         for tag in soup.find_all(href=True): tag["href"] = urljoin(url, tag["href"])
         for tag in soup.find_all(src=True): tag["src"] = urljoin(url, tag["src"])
-        # for tag in soup.find_all(href=True): tag["href"] = self.agent.vision(urljoin(url, tag["href"]))
-        # for tag in soup.find_all(src=True): tag["src"] = self.agent.vision(urljoin(url, tag["src"]))
+        # for tag in soup.find_all('img'): tag.replace_with(self.agent.vision(tag['src']))
 
         text = soup.get_text(strip=True)
-
 
         output_formats = card.json_schema_per_category()
         # Extra parameterization
