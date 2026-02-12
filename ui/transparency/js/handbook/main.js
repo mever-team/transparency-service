e20bc3e09873a4a11c778a72e498275995d7dae6
gnikoul <gnikoul@gmail.com>
2026-02-12 13:18:17 +0200

    handbook menu toggle

--- ui/transparency/js/handbook/main.js
+++ ui/transparency/js/handbook/main.js
@@ -2,37 +2,37 @@ function handbookGoTo(id) {
     document.getElementById(id).scrollIntoView();
 }
 
+$(document).ready(function () {
+    handbookBodyElement = document.getElementById("handbookBody");
+    const bodyIds = Array.from(handbookBodyElement.querySelectorAll('[id]')).map(el => el.id);
+    let previousActiveId = bodyIds[0];
 
-handbookBodyElement = document.getElementById("handbookBody");
-const bodyIds = Array.from(handbookBodyElement.querySelectorAll('[id]')).map(el => el.id);
-let previousActiveId = bodyIds[0];
+    document.addEventListener("scroll", (event) => { getActiveMenuItem(); });
 
-document.addEventListener("scroll", (event) => { getActiveMenuItem(); });
-
-function getActiveMenuItem() {
-    for (let i=bodyIds.length-1; i>-1; i--){
-        const bodyElement = document.getElementById(bodyIds[i]);
-        if (bodyElement.getBoundingClientRect().top - 10<=0) {
-            if (previousActiveId === bodyIds[i]) {return;}
-            document.getElementById(previousActiveId + 'Menu').classList.remove('active');
-            previousActiveId = bodyIds[i];
-            document.getElementById(bodyIds[i] + 'Menu').classList.add('active');
-            return;
+    function getActiveMenuItem() {
+        for (let i=bodyIds.length-1; i>-1; i--){
+            const bodyElement = document.getElementById(bodyIds[i]);
+            if (bodyElement.getBoundingClientRect().top - 10<=0) {
+                if (previousActiveId === bodyIds[i]) {return;}
+                document.getElementById(previousActiveId + 'Menu').classList.remove('active');
+                previousActiveId = bodyIds[i];
+                document.getElementById(bodyIds[i] + 'Menu').classList.add('active');
+                return;
+            }
         }
+        // If all getBoundingClientRect().top >0
+        document.getElementById(previousActiveId + 'Menu').classList.remove('active');
+        previousActiveId = bodyIds[0];
+        document.getElementById(bodyIds[0] + 'Menu').classList.add('active');
     }
-    // If all getBoundingClientRect().top >0
-    document.getElementById(previousActiveId + 'Menu').classList.remove('active');
-    previousActiveId = bodyIds[0];
-    document.getElementById(bodyIds[0] + 'Menu').classList.add('active');
-}
 
 
-let menuIsVisible = true;
-function toggleMenuVisibility() {
-    const handbookMenuEl = document.getElementById('handbookMenu');
-    const hideMenuBtnEl = document.getElementById('hideMenuBtn');
-    if(menuIsVisible) handbookMenuEl.classList.add('hidden');
-    else handbookMenuEl.classList.remove('hidden');
-    hideMenuBtnEl.textContent = menuIsVisible?'▸':'◂';
-    menuIsVisible = !menuIsVisible;
-}
+    $('#hideMenuBtn').on('click', function() {
+        $menu = $('#handbookMenu');
+        $hide_btn = $('#hideMenuBtn')
+
+        $menu.find('div').toggle("slide");
+        $hide_btn.text($hide_btn.text() === '▸'? '◂': '▸');
+    });
+
+});
