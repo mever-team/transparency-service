897f3bec075c75f06db8c58743822f15429d6d73
gnikoul <gnikoul@gmail.com>
2026-03-03 13:05:56 +0200

    chat for full server

--- ui/full_server.py
+++ ui/full_server.py
@@ -1,32 +1,32 @@
-# clone and run
-# > pip install package
-# > python -m ui.test_server
-# examples in tests/server.py (also contains full structure of a model card)
-#
-# .venv serves index.html at http://127.0.0.1:5000
-# server specs in http://127.0.0.1:5000/apidocs
-# refer to the implementation in package/service/server.py
+"""
+clone the repository and run the following:
+> pip install package
+> python -m ui.llama_server
+examples in tests/server.py (also contains full structure of a model card)
+
+.venv serves index.html at http://127.0.0.1:5000
+refer to the implementation in package/service/server.py
+"""
 
 from aicard.service import serve
 from aicard.service.assistants import SemanticMatcher, Prompter, Combined
-from aicard.agents import Ollama
-from threading import Thread
-from aicard.agents.extensions.embeddings import ImageClassifier
 from aicard.service.email import EmailVerification
+from aicard.agents import Ollama
+from aicard.agents.extensions.speedups import text_compression
+from threading import Thread
 
-image_classifier = ImageClassifier()
 matcher = SemanticMatcher(
     "sentence-transformers/all-mpnet-base-v2",
-    external_get_timeout_sec=10,
-    matching_strictness=0.5
-)
+    external_get_timeout_sec=3,
+    matching_strictness=0.5)
 prompter = Prompter(
     Ollama("mistral:latest", name="🌬️ Mistral", timeout_secs=45),
     description="Mistral is used as the base model.",
-    image_classifier=image_classifier
+    text_preprocessor=text_compression
 )
+agent = Combined(refine=prompter, complete=matcher)
 app, gc = serve(
-    {"agent": Combined(complete=matcher, refine=prompter)},
+    {"agent": agent},
     env="ui/.env",
     feature_extractor=matcher,
     email_verification=EmailVerification(env="ui/.env")
@@ -34,4 +34,4 @@ app, gc = serve(
 
 if __name__ == "__main__":
     Thread(target=gc, daemon=True).start()
-    app.run(threaded=False)  # TODO: temporary measure
+    app.run(threaded=False)  # TODO: temporarily mandatory
