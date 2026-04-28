a906bcce3e668bc4d3fc191a21fef37caf960081
gnikoul <gnikoul@gmail.com>
2026-04-28 12:42:16 +0300

    pytest: small changes

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -2,7 +2,7 @@ import time
 def test_import_url(client, admin_token):
     # 1. create card
     url = 'https://arxiv.org/html/2402.19091v2'
-    response = client.post("/transparency/card", json={"title": ""}, headers={"Authorization": f"Bearer {admin_token}"})
+    response = client.post("/transparency/card", json={"title": "pytest_import_url"}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 201
     card_id = int(response.data.decode())
     # 2. start assistant process
@@ -13,6 +13,7 @@ def test_import_url(client, admin_token):
     )
     assert response.status_code == 200
     # 3. poll until finish
+    time.sleep(1) # Give time to start 
     timeout = 30
     start = time.time()
     while True:
@@ -28,7 +29,7 @@ def test_import_url(client, admin_token):
     
 def test_refine(client, admin_token):
     # 1. create card
-    response = client.post("/transparency/card", json={"title": ""}, headers={"Authorization": f"Bearer {admin_token}"})
+    response = client.post("/transparency/card", json={"title": "pytest_refine"}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 201
     card_id = int(response.data.decode())
     # 2. start refine
@@ -38,7 +39,7 @@ def test_refine(client, admin_token):
     )
     assert response.status_code == 200
     # 3. poll until finish
-    time.sleep(5) # Give time for Ollama 
+    time.sleep(2) # Give time for Ollama 
     timeout = 60
     start = time.time()
     while True:
