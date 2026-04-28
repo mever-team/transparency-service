151abfc8f75ed52d5830a52fef5ebf150f9190b8
gnikoul <gnikoul@gmail.com>
2026-04-28 14:07:52 +0300

    pytest: import metrics

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,4 +1,5 @@
 import time
+import json
 def test_import_url(client, admin_token):
     # 1. create card
     url = 'https://arxiv.org/html/2402.19091v2'
@@ -56,3 +57,17 @@ def test_refine(client, admin_token):
     response = client.delete(f"/transparency/card/{card_id}", headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 204
     
+def test_import_metrics(client, admin_token):
+    # 1. create card
+    metrics = {"package_version": "0.1.0",  "date": "2025-Nov-14",  "task": "Text Classification",  "energy_consumption": "0.638 Wh", "metrics": {  "precision_macro": 0.509,  "precision_micro": 0.574,  "recall_macro": 0.464,  "recall_micro": 0.574,  "top1_acc_micro": 0.574,  "top1_acc_macro": 0.574,  "top1_acc_weighted": 0.574,  "f1_macro": 0.466,  "f1_micro": 0.574,  "auc_roc_macro": 0.928,  "auc_roc_weighted": 0.909},  "batch_size": 32,  "hardware": "CPU: AMD Ryzen 7 7800X3D 8-Core Processor, RAM: 15.62 GB, CUDA: | NVIDIA-SMI 580.102.01 Driver Version: 581.57 CUDA Version: 13.0|",  "execution_time": "inference: 34.68s, metrics: 45.76ms",  "num_classes": 28}
+    response = client.post("/transparency/card", json={"title": "pytest_import_url"}, headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 201
+    card_id = int(response.data.decode())
+    # 2. run import metrics
+    response = client.post(f"/transparency/eval_adapter/{card_id}", json=metrics, headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
+    text = json.loads(response.text)
+    assert text['data'][4]['value'][1]['name'] == 'metrics'
+    assert text['data'][4]['value'][1]['value']
+    response = client.delete(f"/transparency/card/{card_id}", headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 204
