175f2dcf696d6ed688ed5f5c2989430ae2172066
gnikoul <gnikoul@gmail.com>
2026-04-29 12:14:20 +0300

    pytest: endpoints done

--- tests/test_card.py
+++ tests/test_card.py
@@ -14,7 +14,7 @@ def test_get_cards(client):
     response = client.post("/transparency/cards", json={})
     assert response.status_code == 200
     
-def test_set_field(client, admin_token, admin_card_id):
+def test_set_card_field(client, admin_token, admin_card_id):
     card_id = admin_card_id
     response = client.put(f"/transparency/card/{card_id}/overview/name", json={"value": "pytest"}, headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
@@ -57,4 +57,58 @@ def test_report(client, admin_token, user_card_id):
     response = client.delete(f"/transparency/reports/{card_id}", headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 204
     assert not response.get_json()
+    
+def test_get_card_fields(client):
+    response = client.get('/transparency/card/fields')
+    assert response.status_code == 200
+    
+def test_get_card_field_names(client):
+    response = client.get('/transparency/card/fields/overview')
+    assert response.status_code == 200
+    
+def test_get_card_field(client, admin_card_id):
+    card_id = admin_card_id
+    response = client.get(f'/transparency/card/{card_id}/overview/name')
+    assert response.status_code == 200
+    
+def test_update_card(client, admin_token, admin_card_id):
+    card_id = admin_card_id
+    response = client.get(f"/transparency/card/{card_id}")
+    assert response.status_code == 200
+    card_json = response.get_json()
+    response = client.put(f"/transparency/card/{card_id}", json=card_json, headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
+    
+# TODO: unclear what json data should send
+def test_update_card_simple(client, admin_token, admin_card_id):
+    card_id = admin_card_id
+    response = client.get(f"/transparency/card/simple/{card_id}")
+    assert response.status_code == 200
+    card_json = response.get_json()
+    response = client.put(f"/transparency/card/simple/{card_id}", json=card_json, headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
+    
+def test_card_job(client, admin_token, admin_card_id):
+    card_id = admin_card_id
+    response = client.get(f"/transparency/job/{card_id}", headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
+    
+def test_download_card(client, admin_card_id):
+    card_id = admin_card_id
+    response = client.get(f"/transparency/card/{card_id}/download/json")
+    assert response.status_code == 200
+    # TODO: aborts with 500
+    response = client.get(f"/transparency/card/{card_id}/download/markdown")
+    assert response.status_code == 200
+    response = client.get(f"/transparency/card/{card_id}/download/pdf")
+    assert response.status_code == 200
+    
+    
+def test_get_options(client):
+    response = client.get(f"/transparency/options/overview/task")
+    assert response.status_code == 200
+    response = client.get(f"/transparency/options/overview/type")
+    assert response.status_code == 200
+    response = client.get(f"/transparency/options/overview/name")
+    assert response.status_code == 400
     
--- tests/test_users.py
+++ tests/test_users.py
@@ -8,9 +8,22 @@ def test_login(client):
     
 def test_user_register_delete(client, admin_token):
     username = 'pytest_user_register_delete'
-    email = 'pytest@example.com'
+    email = 'pytest_user_register_delete@example.com'
     response = client.post('/transparency/register', json={"username": username, "password": "", 'email': email})
     assert response.status_code == 201
     response = client.delete(f'/transparency/users/{username}', headers={"Authorization": f"Bearer {admin_token}"})
     assert response.status_code == 200
     
+def test_update_password(client, user_token):
+    response = client.post('/transparency/update_password', headers={"Authorization": f"Bearer {user_token}"}, json={"password": "newpass"})
+    assert response.status_code == 200
+    
+def test_promote_user(client, admin_token):
+    username = 'pytest_promote_user'
+    email = 'pytest_promote_user@example.com'
+    response = client.post('/transparency/register', json={"username": username, "password": "", 'email': email})
+    assert response.status_code == 201
+    response = client.post(f'/transparency/users/{username}/accept', headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
+    response = client.delete(f'/transparency/users/{username}', headers={"Authorization": f"Bearer {admin_token}"})
+    assert response.status_code == 200
