82431157a7b2b7c88bc3f0fcd4951470cc69e5ab
maniospas <maniospas@hotmail.com>
2026-05-12 20:50:58 +0300

    increased coverage and fixed client code

--- examples/client_demo.py
+++ examples/client_demo.py
@@ -1,8 +1,22 @@
 import aicard as aic
+import multiprocessing
+import time
 
 
-conn = aic.connect("http://127.0.0.1:5000", username="admin", password="admin")
+def start_server():
+    app, gc, monitor = aic.service.serve(root="", admin_username="admin", admin_password="admin", silent=True)
+    app.run(threaded=False)
+multiprocessing.Process(target=start_server, daemon=True).start()
+time.sleep(2)
+
+conn = aic.connect("http://127.0.0.1:5000", username="admin", password="admin", silent=True)
+
 with conn.create() as card:
-    card.title = "Updated model card name"
+    card.title = "my new card"
 
-print(len(conn.search("updated model card", top=100)))
+with conn.attach(1) as card:
+    assert card.title == "my new card"
+    card.title = "edited title"
+
+with conn.attach(1) as card:
+    assert card.title == "edited title"
